87d394a4084bf967bb77ba158be2d3f9ce394fc6
Umair Zaki <umairzakicnbc@gmail.com>
2020-01-03 06:48:50 -0500

    Piaic Website

--- src/pages/wit.js
+++ src/pages/wit.js
@@ -1,8 +1,139 @@
 import React from "react"
 import Layout from "../components/layout"
+import WIT from "../components/WIT.module.scss"
 
 
 
 export default () => <Layout>
-    <h1>WIT page</h1>
+  <div className={WIT.super}>
+  <p>The Women Empowerment Division of the Presidential Initiative for Artificial Intelligence & Computing is committed to the empowerment and autonomy of women in Pakistan.</p>
+  <p>The single most important channel for the empowerment of women is education. The Women Empowerment Division is committed to providing the best technical education in the world with the flexibility to allow just about anyone to participate and improve their lives. The primary objective of the Women Empowerment Division is to provide a platform via which women can receive education and training to jumpstart their careers in the global technology industry.</p>
+  <p>The Women Inclusion in Technology project is the largest initiative on women education in Pakistan. The main of the project is to drive female participation in the technology industry in Pakistan from less than 5% to 30% over the next two years. Via this project, PIAIC is propagating awareness and training on the latest, most state-of-the-art technologies to women in Pakistan. The Women Empowerment Division will aid highly skilled women in technology to establish themselves as entrepreneurs and leaders in the technology industry not only in Pakistan but also around the world.</p>
+
+  <h1>WOMEN INCLUSION IN TECHNOLOGY</h1>
+  <div className={WIT.line}></div>
+  <h2>PIAIC WOMEN'S GALLERY</h2>
+  
+  <div className={WIT.spotlight}>
+  <div className={WIT.image}></div>
+  <div className={WIT.image2}></div>
+  <div className={WIT.image3}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image4}></div>
+  <div className={WIT.image5}></div>
+  <div className={WIT.image6}></div>
+  </div>
+  
+  <div className={WIT.spotlight}>
+  <div className={WIT.image7}></div>
+  <div className={WIT.image8}></div>
+  <div className={WIT.image9}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image10}></div>
+  <div className={WIT.image11}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image12}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image13}></div>
+  <div className={WIT.image14}></div>
+  <div className={WIT.image15}></div>
+  <div className={WIT.image17}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image18}></div>
+  <div className={WIT.image19}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image20}></div>
+  <div className={WIT.image21}></div>
+  <div className={WIT.image22}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image23}></div>
+  <div className={WIT.image24}></div>
+  <div className={WIT.image25}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image26}></div>
+  <div className={WIT.image27}></div>
+  <div className={WIT.image28}></div>
+  <div className={WIT.image29}></div>
+  <div className={WIT.image30}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image31}></div>
+  <div className={WIT.image32}></div>
+  <div className={WIT.image33}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image34}></div>
+  <div className={WIT.image35}></div>
+  <div className={WIT.image36}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image37}></div>
+  <div className={WIT.image38}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image39}></div>
+  <div className={WIT.image40}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image41}></div>
+  <div className={WIT.image42}></div>
+  <div className={WIT.image43}></div>
+  <div className={WIT.image44}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image45}></div>
+  <div className={WIT.image46}></div>
+  <div className={WIT.image47}></div>
+  <div className={WIT.image48}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image49}></div>
+  <div className={WIT.image50}></div>
+  <div className={WIT.image51}></div>
+  </div>
+  
+  <div className={WIT.spotlight}>
+  <div className={WIT.image52}></div>
+  </div>
+  
+  <div className={WIT.spotlight}>
+  <div className={WIT.image53}></div>
+  <div className={WIT.image54}></div>
+  <div className={WIT.image55}></div>
+  </div>
+
+  <div className={WIT.spotlight}>
+  <div className={WIT.image56}></div>
+  <div className={WIT.image57}></div>
+  <div className={WIT.image58}></div>
+  <div className={WIT.image59}></div>
+  </div>
+  
+  </div>
+  
+
+
 </Layout>
